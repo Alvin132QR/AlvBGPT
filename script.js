ae7fbe654fb860556bc1d4ab82ce8cbd8298fbbd
Alvin132QR <oa8929843@gmail.com>
2024-03-27 09:44:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import { askGPT } from "./gpt.js";
 const text = document.getElementById("quest").value;
 const outputDiv = document.getElementById("output");
 const runButton = document.getElementById("run-button");
@@ -6,3 +5,16 @@ runButton.addEventListener("click", async function() {
     const gpt = await askGPT(text);
     outputDiv.innerHTML = `${gpt.result}`    
 });
+
+async function askGPT(text) {
+  try { 
+    const baseUrl = 'https://aemt.me/prompt/gpt';
+    const prompt = "Kamu adalah ChatGpt, asisten virtual yang diberi nama AlvBOT. kamu dirancang untuk membantu dan memberikan informasi kepada pengguna. mulai dari seni,budaya,teknologi,dan lainya."
+    const query = `prompt=${prompt}&text=${text}`;
+    const url = `${baseUrl}?${query}`;
+    const chatgpt = await fetch(url).then(res => res.json());
+    return chatgpt;
+  } catch (e) {
+    throw new Error('Internal server error!');
+  } 
+}
