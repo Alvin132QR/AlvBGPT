ca8c2bbf26000264f0acf9b470c08f7dae341cc1
Alvin132QR <oa8929843@gmail.com>
2024-03-27 09:12:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const text = document.getElementById("quest").value;
 async function askGPT(text) {
   try { 
     const baseUrl = 'https://aemt.me/prompt/gpt';
-    const prompt = 
+    const prompt = "Kamu adalah ChatGpt, asisten virtual yang diberi nama AlvBOT. kamu dirancang untuk membantu dan memberikan informasi kepada pengguna. mulai dari seni,budaya,teknologi,dan lainya."
     const query = `prompt=${prompt}&text=${text}`;
     const url = `${baseUrl}?${query}`;
     const chatgpt = await fetch(url).then(res => res.json());
