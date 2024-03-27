1fe0c6de36bbf4b491541da891eaad19b4d75c10
Alvin132QR <oa8929843@gmail.com>
2024-03-27 08:52:11 +0700

    Update lib.js

--- lib.js
+++ lib.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 async function askGPT(text) {
   try { 
     const baseUrl = 'https://aemt.me/prompt/gpt';
-    const prompt = fs.readFileSync('./lib/prompt.txt', 'utf-8');
+    const prompt = fs.readFileSync('./prompt.txt', 'utf-8');
     const query = `prompt=${prompt}&text=${text}`;
     const url = `${baseUrl}?${query}`;
     const chatgpt = await fetch(url).then(res => res.json());
