22cd2bddd926e1c737128adc5b2f343c4bb1d260
Alvin132QR <oa8929843@gmail.com>
2024-03-27 09:37:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const text = document.getElementById("quest").value;
     const runButton = document.getElementById("run-button");
     runButton.addEventListener("click", async function() {
       const gpt = await askGPT(text);
-      outputDiv.innerHTML = `${gpt.result}'
+      outputDiv.innerHTML = `${gpt.result}`
         
     });
 
