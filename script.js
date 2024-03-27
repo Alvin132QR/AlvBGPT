3a8f88360b1e375032329e76b01d360b2438385d
Alvin132QR <oa8929843@gmail.com>
2024-03-27 09:10:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-import { askGPT } from './lib.js';
 const text = document.getElementById("quest").value;
     const outputDiv = document.getElementById("output");
     const runButton = document.getElementById("run-button");
@@ -6,3 +5,16 @@ const text = document.getElementById("quest").value;
       var res = await askGPT(text);
       outputDiv.innerHTML = `{res}`;
     });
+
+async function askGPT(text) {
+  try { 
+    const baseUrl = 'https://aemt.me/prompt/gpt';
+    const prompt = 
+    const query = `prompt=${prompt}&text=${text}`;
+    const url = `${baseUrl}?${query}`;
+    const chatgpt = await fetch(url).then(res => res.json());
+    return chatgpt;
+  } catch (e) {
+    throw new Error('Internal server error!');
+  } 
+}
